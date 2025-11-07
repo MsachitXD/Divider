ac2bc1bc5a031a3fa0dd947aa1d25a9234f99d29
Sachit Mittal <sachitmittal073@gmail.com>
2025-11-07 12:20:41 +0530

    Initial Commit

---
+++ Divider View/Divider_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Divider_ViewApp.swift
+//  Divider View
+//
+//  Created by Sachit Mittal on 07/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Divider_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Divider ViewTests/Divider_ViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  Divider_ViewTests.swift
+//  Divider ViewTests
+//
+//  Created by Sachit Mittal on 07/11/25.
+//
+
+import Testing
+@testable import Divider_View
+
+struct Divider_ViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
